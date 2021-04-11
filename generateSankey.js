0f7b74363158303f4e89d75db85c28183bc3d047
Philip <philip.nguyen@sjsu.edu>
2021-04-11 15:39:42 -0700

    first commit

---
+++ generateSankey.js
@@ -0,0 +1,9 @@
+// load in the Google Chart sankey diagram API
+google.charts.load('current', {'packages':['sankey']});
+google.charts.setOnLoadCallback(generateSankey);
+
+var data = $.csv.toObject
+
+function generateSankey() {
+
+}
