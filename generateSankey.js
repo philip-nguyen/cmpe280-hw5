7025b4db28cdff49269f251db780645cc67b1458
Philip <philip.nguyen@sjsu.edu>
2021-04-12 22:17:48 -0700

    generates sankey diagram now

--- generateSankey.js
+++
@@ -1,9 +0,0 @@
-// load in the Google Chart sankey diagram API
-google.charts.load('current', {'packages':['sankey']});
-google.charts.setOnLoadCallback(generateSankey);
-
-var data = $.csv.toObject
-
-function generateSankey() {
-
-}
